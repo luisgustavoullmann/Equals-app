81e28d2291036e6fbeb784b56f8437320da2ed0c
Luis Gustavo Ullmann <luisgustavoullmann@gmail.com>
2019-07-24 11:32:56 -0300

    Listando pedidos para empresa - v1.1

--- app/src/main/java/br/com/project/equals/activity/PedidosActivity.java
+++ app/src/main/java/br/com/project/equals/activity/PedidosActivity.java
@@ -5,13 +5,34 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
 import br.com.project.equals.R;
+import br.com.project.equals.adapter.AdapterPedido;
+import br.com.project.equals.adapter.AdapterProduto;
+import br.com.project.equals.helper.ConfiguracaoFirebase;
+import br.com.project.equals.helper.UsuarioFirebase;
+import br.com.project.equals.model.Pedido;
+import br.com.project.equals.model.Usuario;
+import dmax.dialog.SpotsDialog;
 
 public class PedidosActivity extends AppCompatActivity {
 
     private RecyclerView recyclerPedidos;
+    private AdapterPedido adapterPedido;
+    private List<Pedido> pedidos;
+    private AlertDialog dialog;
+    private DatabaseReference firebaseRef;
+    private String idEmpresa;
 
     @SuppressLint("RestrictedApi")
     @Override
@@ -21,6 +42,8 @@ public class PedidosActivity extends AppCompatActivity {
         
         //Configurações iniciais
         inicializarComponentes();
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
+        idEmpresa = UsuarioFirebase.getIdUsuario();
 
         //Setup da Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -30,11 +53,58 @@ public class PedidosActivity extends AppCompatActivity {
         getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
 
         //Configurando Recycler View - ver AdapterPedidos, MyViewHolder
+        recyclerPedidos = findViewById(R.id.recyclerPedidos);
+        recyclerPedidos.setHasFixedSize(true);
+        adapterPedido = new AdapterPedido(pedidos);
+        recyclerPedidos.setAdapter(adapterPedido);
 
+        recuperarPedidos();
 
     }
 
     private void inicializarComponentes() {
         recyclerPedidos = findViewById(R.id.recyclerPedidos);
+    }
+
+    private void recuperarPedidos(){
+        dialog = new SpotsDialog.Builder()
+                .setContext(this)
+                .setMessage("Carregando dados")
+                .setCancelable(false)
+                .build();
+        dialog.show();
+
+        DatabaseReference pedidoRef = firebaseRef
+                .child("pedidos")
+                .child(idEmpresa);
+
+        //Recuperando os pedidos com determinado status,
+        //confirmado no caso
+        Query pedidoPesquisa = pedidoRef.orderByChild("status")
+                .equalTo("confirmado");
+
+        pedidoPesquisa.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                pedidos.clear();
+                if(dataSnapshot.getValue() != null){
+                    for(DataSnapshot ds : dataSnapshot.getChildren()){
+                        Pedido pedido = ds.getValue(Pedido.class);
+                        pedidos.add(pedido);
+                    }
+                    //Notificando o adapter
+                    adapterPedido.notifyDataSetChanged();
+                    dialog.dismiss();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
