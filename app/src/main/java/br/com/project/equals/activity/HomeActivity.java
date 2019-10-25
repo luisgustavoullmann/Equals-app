7fa8ac5cac8f50887519eeec129172cbb91b8d84
Luis Gustavo Ullmann <luisgustavoullmann@gmail.com>
2019-10-25 16:02:00 -0300

    Beacon - veriosn 1.4

--- app/src/main/java/br/com/project/equals/activity/HomeActivity.java
+++ app/src/main/java/br/com/project/equals/activity/HomeActivity.java
@@ -11,9 +11,12 @@ import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.graphics.Region;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -30,6 +33,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import br.com.project.equals.R;
@@ -39,9 +43,10 @@ import br.com.project.equals.listener.RecyclerItemClickListener;
 import br.com.project.equals.model.Empresa;
 import br.com.project.equals.model.Produto;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implementes BeaconConsumer {
 
     private static final String TAG = "HomeActivity";
+    private BeaconManager beaconManager;
     private FirebaseAuth autenticacao;
     private MaterialSearchView searchView;
     private RecyclerView recyclerEmpresa;
@@ -276,13 +281,30 @@ public class HomeActivity extends AppCompatActivity {
             }
         }
 
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        //Para detectar beacons proprietarios (como estimote), você precisa adicionar a linha do layout correspondente
+        // ao tipo de beacon. Procure por "setBeaconLayout" para buscar outros exemplos
+        // No exemplo abaixo, está funcionando para buscar os beacons da estimote (possuem exemplo no aplicativo de simular
+        //beacons
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.bind(this);
+
+        @Override
+        public void onDestroy(){
+            super.onDestroy();
+            beaconManager.unbind(this);
+        }
+
+
         @Override
         public void onRequestPermissionsResult(int requestCode,
         String permissions[], int[] grantResults) {
             switch (requestCode) {
                 case PERMISSION_REQUEST_FINE_LOCATION: {
                     if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        Log.d(TAG, "Aceita a permissão de localização");
+                        Log.d(TAG, "Permissão aceita");
                     } else {
                         final AlertDialog.Builder builder = new AlertDialog.Builder(this);
                         builder.setTitle("Funcionalidade Limitada");
@@ -318,6 +340,104 @@ public class HomeActivity extends AppCompatActivity {
                     }
                     return;
                 }
+            }
+        }
+
+        @Override
+        public void unbindService(ServiceConnection serviceConnection){
+            unbindService(serviceConnection);
+        }
+
+        @Override
+        public void onBeaconServiceConnection(){
+            //Constroi nova região para monitorar
+            final Region region = new Region("myBeacons", null, null, null);
+
+            //Adciona uma notificação para ser chamada
+            beaconManager.addMonitorNotifier(new MonitorNotifier(){
+                /*
+                This override method is runned when some beacon will come under the range of device.
+            */
+                @Override
+                public void didEnterRegion(Region region) {
+                    System.out.println("----- ENTER -----");
+                    try {
+                        //Avisa que achou um beacon
+                        beaconManager.startRangingBeaconsInRegion(region);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                /*
+                     This override method is runned when beacon that comes in the range of device
+                     ,now been exited from the range of device.
+                 */
+                @Override
+                public void didExitRegion(Region region) {
+                    System.out.println("----- EXIT -----");
+                    try {
+
+                        //Avisa que o beacon parou de ser lido
+                        beaconManager.stopRangingBeaconsInRegion(region);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+                /*
+                     Este metodo determina o estado do serviço 0 - desligado | 1 - ligado
+                */
+                @Override
+                public void didDetermineStateForRegion(int state, Region region) {
+                    System.out.println( "I have just switched from seeing/not seeing beacons: "+state);
+                }
+            });
+
+            //Adiciona a notificação para ser chamada ser BeaconService pegou uma informação
+            beaconManager.addRangeNotifier(new RangeNotifier() {
+
+                /*
+                   This Override method tells us all the collections of beacons and their details that
+                   are detected within the range by device
+                 */
+                @Override
+                public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+
+
+                    // Se o Beacon é encontrado, o tamanho é maior que 0
+                    if (beacons.size() > 0) {
+                        final ArrayList<ArrayList<String>> arrayList = new ArrayList<ArrayList<String>>();
+
+                        // Pega a informação de todos os beacons encontrados
+                        for (Beacon b:beacons){
+
+                            //UUID
+                            String uuid = String.valueOf(b.getId1());
+
+                            //Major
+                            String major = String.valueOf(b.getId2());
+
+                            //Minor
+                            String minor = String.valueOf(b.getId3());
+
+                            Log.v("Test",uuid + " " + major + " " + minor);
+
+                        }
+                    }
+                    // Se nenhum beacon foi encontrado, retorna 0
+                    else if (beacons.size()==0) {
+
+                    }
+                }
+            });
+
+            try{
+                //Iniciando monitoramento dos beacons
+                beaconManager.startMonitorBeaconsInRegion(region);
+            } catch (RemoteException e){
+                //
             }
         }
     }
---
+++ app/src/main/java/br/com/project/equals/model/MainApplication.java
@@ -0,0 +1,120 @@
+package br.com.project.equals.model;
+
+import android.app.Application;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.graphics.Region;
+import android.widget.Toast;
+
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+import org.altbeacon.beacon.service.BeaconService;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+
+public class MainApplication extends Application implements BootstrapNotifier {
+
+    public static BeaconManager beaconManager;
+    private RegionBootstrap regionBootstrap;
+    private BackgroundPowerSaver backgroundPowerSaver;
+    public static Region region1;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Pegando o objeto bluetooth
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // Checka se o bluetooth é suportado pelo device
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(getApplicationContext(),"Bluetooth Not Supported",Toast.LENGTH_LONG).show();
+        } else {
+            // Se o bluetooth é suportado e não está habilitado, pede para habilitar
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent bluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                bluetoothIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(bluetoothIntent);
+            }
+        }
+
+        // Pega a Application do BeaconManager
+        beaconManager = org.altbeacon.beacon.BeaconManager.getInstanceForApplication(this);
+
+        //Para detectar beacons proprietarios (como estimote), você precisa adicionar a linha do layout correspondente
+        // ao tipo de beacon. Procure por "setBeaconLayout" para buscar outros exemplos
+        // No exemplo abaixo, está funcionando para buscar os beacons da estimote (possuem exemplo no aplicativo de simular
+        //beacons
+        beaconManager.getBeaconParsers().add(new BeaconParser()
+                .setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+
+        //Altera o default de tempo que os beacons são procuradso
+        beaconManager.setForegroundScanPeriod(1100l);
+        beaconManager.setForegroundBetweenScanPeriod(0l);
+
+        //Se estiver usando um device API 21+m usa a API Android L para buscar beacons
+        beaconManager.setAndroidLScanningDisabled(true);
+
+        //Seta a duração em milisegundos para o ciclo de busca BLE quando o clientes não estão em foreground
+        beaconManager.setBackgroundBetweenScanPeriod(01);
+        beaconManager.setBackgroundScanPeriod(1100l);
+
+        try {
+            //Altera as informações
+            beaconManager.updateScanPeriods();
+        } catch (Exception e) {
+        }
+
+        // acorda quando o beacon é acordado
+        region1 = new Region("backgroundRegion",
+                null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region1);
+        backgroundPowerSaver = new BackgroundPowerSaver(this);
+    }
+
+    /*
+        //Avisa que achou um beacon
+     */
+    @Override
+    public void didEnterRegion(Region region) {
+        try {
+
+            //Iniciando BeaconService que extende a classe serviceService
+            Intent i = new   Intent(getApplicationContext(), BeaconService.class);
+            startService(i);
+        } catch (Exception e){}
+    }
+
+    /*
+        Avisa que o beacon parou de ser lido
+     */
+    @Override
+    public void didExitRegion(Region region) {
+
+        try {
+            //Iniciando BeaconService que extende a classe serviceService
+            Intent k = new Intent(getApplicationContext(),BeaconService.class);
+            startService(k);
+        }
+        catch (Exception e) {
+        }
+
+    }
+
+    /*
+      Este metodo determina o estado do serviço 0 - desligado | 1 - ligado
+     */
+    @Override
+    public void didDetermineStateForRegion(int i, Region region) {
+
+        try {
+            //Iniciando BeaconService que extende a classe serviceService
+            Intent k = new Intent(getApplicationContext(), BeaconService.class);
+            startService(k);
+        }
+        catch (Exception e) {
+        }
+    }
+
+}
